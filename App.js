7a5f374c015f7437f23b2f85594de6282ce3ef48
canewman <54854772+canewman@users.noreply.github.com>
2021-06-21 16:22:31 -0500

    Successfully pulls data from API
    Successfully pulls the data from the API, I cannot however get it to parse the data correctly

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Header from './header.js';
 import Footer from './footer.js';
 import Card from './card.js';
 
-var address = 'https://api.currencyscoop.com/v1/currencies?api_key=a069d8f602f2ba4be081d0b4700910e7&type=fiat'
+//var address = 'https://api.currencyscoop.com/v1/currencies?api_key=a069d8f602f2ba4be081d0b4700910e7&type=fiat'
 
-function App() {
-  return (
-    <div className="App">
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []      
+    };
+  }
+
+  componentDidMount() {
+    const apiUrl = 'https://api.currencyscoop.com/v1/currencies?api_key=a069d8f602f2ba4be081d0b4700910e7&type=fiat';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        var lol = data
+        var nextData = []
+
+        function printValues(obj) {
+          for(var k in obj) {
+              if(obj[k] instanceof Object) {
+                  printValues(obj[k]);
+              } else {                  
+                  nextData.push(obj[k]);
+              };
+          }
+      };
+
+      //printValues(lol)
+      //nextData.splice(0, 2)
+      //JSON.parse(nextData)
+      //console.log(nextData)
+      
+        this.setState({data: lol})
+      });
+  }
+  render() {
+
+    console.log(this.state.data.currency_name)
+    return (
+      <div className="App">
       <header className="App-header">
         <Header />
       </header>
@@ -17,7 +54,37 @@ function App() {
         <Footer />
       </footer>
     </div>
+    );
+  }
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+      </header>
+      <Api />
+      <Card />
+      <footer>
+        <Footer />
+      </footer>
+    </div>
   );
 }
 
+
+class App extends React.Component {
+  componentDidMount() {
+    const apiUrl = 'https://api.currencyscoop.com/v1/currencies?api_key=a069d8f602f2ba4be081d0b4700910e7&type=fiat';
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
+}
+*/
 export default App;
